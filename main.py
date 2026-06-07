81259a209184a13d2644cf87e9c89145485f4a00
xtzx402 <wenli5741@gmail.com>
2026-06-06 21:07:46 -0400

    update for green version

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 
 app = FastAPI()
 
-VERSION = os.getenv("VERSION", "blue")
+VERSION = os.getenv("VERSION", "green")
 
 @app.get("/")
 def root():
-    return {"message": "Server is running", "version": VERSION}
+    return {"message": "Server is running - v2", "version": VERSION}
 
 @app.get("/health")
 def health():
